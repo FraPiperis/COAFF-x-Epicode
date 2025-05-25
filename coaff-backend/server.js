835b05843cc24cf16da2b194620d6b221de4c2f2
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-05-25 11:23:31 +0200

    Backend

--- coaff-backend/server.js
+++ coaff-backend/server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log('✅ Connesso a MongoDB locale'))
+  .catch(err => console.error('❌ Errore di connessione:', err));
+  
 dotenv.config();
 connectDB();
 
